0ba1f6b82ca907b6a94fae3b0aa8d06bbaf50d26
PhilipGough <pgough@redhat.com>
2018-08-24 16:12:40 +0100

    Update sdk version and related, required code changes

--- ostia-operator/pkg/apicast/reconcile.go
+++ ostia-operator/pkg/apicast/reconcile.go
@@ -1,11 +1,11 @@
 package apicast
 
 import (
-	"github.com/3scale/ostia/ostia-operator/pkg/apis/ostia/v1alpha1"
-	"github.com/operator-framework/operator-sdk/pkg/sdk/action"
-	"github.com/operator-framework/operator-sdk/pkg/sdk/query"
-	log "github.com/sirupsen/logrus"
 	"reflect"
+
+	"github.com/3scale/ostia/ostia-operator/pkg/apis/ostia/v1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	log "github.com/sirupsen/logrus"
 )
 
 //Reconcile takes care of the main apicast reconciliation loop
@@ -41,13 +41,13 @@ func reconcileDeploymentConfig(api *v1alpha1.API) (err error) {
 	existingDc := DeploymentConfig(api)
 	desiredDc := DeploymentConfig(api)
 
-	err = query.Get(existingDc)
+	err = sdk.Get(existingDc)
 	if err != nil {
-		err = action.Create(desiredDc)
+		err = sdk.Create(desiredDc)
 	} else {
 		if !reflect.DeepEqual(existingDc.Spec, desiredDc.Spec) {
 			existingDc.Spec = desiredDc.Spec
-			err = action.Update(existingDc)
+			err = sdk.Update(existingDc)
 		}
 	}
 
@@ -59,13 +59,13 @@ func reconcileService(api *v1alpha1.API) (err error) {
 	existingSvc := Service(api)
 	desiredSvc := Service(api)
 
-	err = query.Get(existingSvc)
+	err = sdk.Get(existingSvc)
 	if err != nil {
-		err = action.Create(desiredSvc)
+		err = sdk.Create(desiredSvc)
 	} else {
 		if !reflect.DeepEqual(existingSvc.Spec.Ports, desiredSvc.Spec.Ports) {
 			existingSvc.Spec.Ports = desiredSvc.Spec.Ports
-			err = action.Update(existingSvc)
+			err = sdk.Update(existingSvc)
 		}
 	}
 	return err
@@ -77,14 +77,14 @@ func reconcileRoute(api *v1alpha1.API) (err error) {
 	existingRoute := Route(api)
 	desiredRoute := Route(api)
 
-	err = query.Get(existingRoute)
+	err = sdk.Get(existingRoute)
 	if err != nil {
-		err = action.Create(desiredRoute)
+		err = sdk.Create(desiredRoute)
 	} else {
 
 		if !reflect.DeepEqual(existingRoute.Spec, desiredRoute.Spec) {
 			existingRoute.Spec = desiredRoute.Spec
-			err = action.Update(existingRoute)
+			err = sdk.Update(existingRoute)
 		}
 	}
 
