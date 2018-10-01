9f34447cb129dc9670eb8a5bf6634f61f26f37ca
Joaquim Moreno Prusi <jmprusi@keepalive.io>
2018-10-01 15:33:34 +0200

    Adds ThreescaleAPI reconciliation libraries
    Co-authored-by: Philip Gough <pgough@redhat.com>

--- threescaleAPI-operator/cmd/threescaleAPI/main.go
+++ threescaleAPI-operator/cmd/threescaleAPI/main.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 	"time"
 
-	stub "github.com/3scale/ostia/threescaleAPI-operator/pkg/stub"
+	threescaleAPI "github.com/3scale/ostia/threescaleAPI-operator/pkg/threescaleAPI"
 	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
 	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
@@ -34,6 +34,6 @@ func main() {
 	resyncPeriod := time.Duration(5) * time.Second
 	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
 	sdk.Watch(resource, kind, namespace, resyncPeriod)
-	sdk.Handle(stub.NewHandler())
+	sdk.Handle(threescaleAPI.NewHandler())
 	sdk.Run(context.TODO())
 }
