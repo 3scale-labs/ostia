f5ebaec24bae22f4bb5cbe3a0488598aabae4ce4
Michal Cichra <michal@cichra.cz>
2020-10-22 14:53:11 +0200

    rubocop and standardrb

--- auth-ruby/auth.rb
+++ auth-ruby/auth.rb
@@ -22,44 +22,43 @@ module GRPC
   extend RubyLogger
 end
 
-$rate_limits = Hash.new {|hash, key| hash[key] = rand(60) }
+$rate_limits = Hash.new { |hash, key| hash[key] = rand(60) }
 
 Envoy::Service::Auth::V2::AttributeContext::HttpRequest.module_eval do
   def to_env
-    headers.to_h.delete_if {|k,_| k.start_with?(':') }.transform_keys { |k| "HTTP_#{k.tr('-', '_').upcase}" }
+    headers.to_h.delete_if { |k, _| k.start_with?(':') }.transform_keys { |k| "HTTP_#{k.tr('-', '_').upcase}" }
   end
 end
 
 class V3AuthorizationService < Envoy::Service::Auth::V3::Authorization::Service
-
   def check(check, rest)
     puts 'check', check, rest
-    puts  check.to_json(emit_defaults: true), check.class.name
+    puts check.to_json(emit_defaults: true), check.class.name
     Envoy::Service::Auth::V3::CheckResponse.new(
       status: Google::Rpc::Status.new(code: GRPC::Core::StatusCodes::OK),
       ok_response: Envoy::Service::Auth::V3::OkHttpResponse.new(
         headers: [
           Envoy::Config::Core::V3::HeaderValueOption.new(header: Envoy::Config::Core::V3::HeaderValue.new(key: 'x-ext-auth-ratelimit', value: $rate_limits[user].to_s))
-        ])
+        ]
+      )
     )
   end
 end
 
-
 class V2AuthorizationService
-    include GRPC::GenericService
+  include GRPC::GenericService
 
-    self.marshal_class_method = :encode
-    self.unmarshal_class_method = :decode
-    self.service_name = 'envoy.service.auth.v2.Authorization'
+  self.marshal_class_method = :encode
+  self.unmarshal_class_method = :decode
+  self.service_name = 'envoy.service.auth.v2.Authorization'
 
-    # Performs authorization check based on the attributes associated with the
-    # incoming request, and returns status `OK` or not `OK`.
-    rpc :Check, Envoy::Service::Auth::V2::CheckRequest, Envoy::Service::Auth::V2::CheckResponse
+  # Performs authorization check based on the attributes associated with the
+  # incoming request, and returns status `OK` or not `OK`.
+  rpc :Check, Envoy::Service::Auth::V2::CheckRequest, Envoy::Service::Auth::V2::CheckResponse
 
   def check(check, rest)
     puts 'check', check, rest
-    puts  check.to_json(emit_defaults: true), check.class.name
+    puts check.to_json(emit_defaults: true), check.class.name
     auth = Rack::Auth::Basic::Request.new(check.attributes.request.http.to_env)
 
     Envoy::Service::Auth::V2::CheckResponse.new(
@@ -68,23 +67,24 @@ class V2AuthorizationService
         headers: [
           Envoy::Api::V2::Core::HeaderValueOption.new(header: Envoy::Api::V2::Core::HeaderValue.new(key: 'x-ext-auth-ratelimit', value: $rate_limits[auth.username].to_s)),
           Envoy::Api::V2::Core::HeaderValueOption.new(header: Envoy::Api::V2::Core::HeaderValue.new(key: 'x-ext-auth-user', value: auth.username))
-        ])
+        ]
+      )
     )
   end
 end
 
 def main
-port = '0.0.0.0:50051'
-s = GRPC::RpcServer.new
-s.add_http2_port(port, :this_port_is_insecure)
-GRPC.logger.info("... running insecurely on #{port}")
-s.handle(V2AuthorizationService.new())
-s.handle(V3AuthorizationService.new())
+  port = '0.0.0.0:50051'
+  s = GRPC::RpcServer.new
+  s.add_http2_port(port, :this_port_is_insecure)
+  GRPC.logger.info("... running insecurely on #{port}")
+  s.handle(V2AuthorizationService.new)
+  s.handle(V3AuthorizationService.new)
 
-# Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to
-#   gracefully shutdown.
-# User could also choose to run server via call to run_till_terminated
-s.run_till_terminated_or_interrupted([1, +'int', +'SIGQUIT'])
+  # Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to
+  #   gracefully shutdown.
+  # User could also choose to run server via call to run_till_terminated
+  s.run_till_terminated_or_interrupted([1, +'int', +'SIGQUIT'])
 end
 
 main if __FILE__ == $0
